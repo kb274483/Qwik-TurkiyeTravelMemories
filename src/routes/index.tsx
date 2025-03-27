0797999aeeb3d1bfcaf71a4502fe53f209c243f1
Roy <roy@gastom.com.tw>
2025-03-27 11:27:24 +0800

    init project and install tailwindcss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,11 +15,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Roy's Travel Memories",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Roy & Buccula's Turkiye Travel Memories",
     },
   ],
 };
