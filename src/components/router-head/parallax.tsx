db75c354a7bed9cd420d62781596138b95b8d58e
Roy <roy@gastom.com.tw>
2025-03-27 18:35:05 +0800

    [adjust] init parallax effect

--- src/components/router-head/parallax.tsx
+++ src/components/router-head/parallax.tsx
@@ -5,50 +5,87 @@ export const Parallax = component$(() => {
 
   useVisibleTask$(() => {
     const el = document.querySelector('.hotairBalloon') as HTMLDivElement;
-
     let t = 0;
     const float = () => {
       t += 0.02;
-
-      const x = Math.sin(t) * 10;
-      const y = Math.cos(t) * 15;
+      const x = Math.sin(t) * 25;
+      const y = Math.cos(t) * 20;
       el.style.transform = `translate(${x}px, ${y}px)`;
 
       requestAnimationFrame(float);
     };
-
     float();
   });
 
+  useVisibleTask$(() => {
+    const scroll = () => {
+      const scrollY = window.scrollY;
+      const hotairBalloon = document.querySelector('.hotairBalloon') as HTMLDivElement;
+      const parallaxItems = document.querySelectorAll('.parallax-item') as NodeListOf<HTMLElement>;
+
+      hotairBalloon.style.transform = `translateY(${scrollY * 0.5}px)`;
+      parallaxItems.forEach((item) => {
+        const speed = item.getAttribute('data-speed');
+        console.log(speed);
+        if (speed) {
+          const y = (scrollY * parseInt(speed)) / 100;
+          item.style.transform = `translateY(-${y}px)`;
+        }
+      })
+    }
+    window.addEventListener('scroll', scroll);
+    return () => {
+      window.removeEventListener('scroll', scroll);
+    }
+  })
+
   return (
-    <div class="parallax bg-amber-700" style={styles.parallax}>
-      <div class="parallax-item hotairBalloon" style={{
-        ...styles.parallaxItem, ...styles.hotairBalloon
-      }}>
-        <img src="/parallaxImg/1.webp" alt="" style={styles.image} />
-      </div>
-      {imgArr.value.map((i) => (
-        <div key={i}
-          class="parallax-item" style={styles.parallaxItem}
+    <>
+      <div class="parallax bg-stone-900" style={styles.parallax}>
+        <div data-speed="200"  
+          class="parallax-item hotairBalloon" 
+          style={{
+            ...styles.parallaxItem, ...styles.hotairBalloon
+          }}
         >
-          <img
-            width={100}
-            height={100}
-            alt="parallaxImg"
-            src={`/parallaxImg/${i}.webp`} 
-            style={styles.image} 
-          />
+          <img src="/parallaxImg/1.webp" alt="" style={styles.image} />
         </div>
-      ))}
-    </div>
+        {imgArr.value.map((i) => (
+          <div key={i}
+            class="parallax-item" 
+            style={{
+              ...styles.parallaxItem,
+              ...(i === 4 ? styles.mountain : {}),
+              ...(i === 5 ? styles.ground : {})
+            }}
+            data-speed={
+              i === 2 ?
+              '50' : i === 3 ? 
+              '100' : i === 4 ? 
+              '150' : '200'
+            }
+          >
+            <img
+              width={100}
+              height={100}
+              alt="parallaxImg"
+              src={`/parallaxImg/${i}.webp`} 
+              style={styles.image} 
+            />
+          </div>
+        ))}
+      </div>
+      <div class="bg-stone-900 h-[150dvh]"></div>
+    </>
   );
+
 });
 
 const styles = {
   parallax: {
     position: "relative" as const,
     width: "100%",
-    height: "100vh",
+    height: "150dvh",
     overflow: "hidden",
   },
   parallaxItem: {
@@ -57,10 +94,15 @@ const styles = {
     left: "0",
     right: "0",
     bottom: "0",
-    zIndex: "1",
+  },
+  mountain: {
+    zIndex: "10",
+  },
+  ground: {
+    zIndex: "15",
   },
   hotairBalloon: {
-    zIndex: "10",
+    zIndex: "5",
   },
   image: {
     width: "100%",
