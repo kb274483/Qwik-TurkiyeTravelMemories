8ad942043eb80966009ccb317c8c75786df2831f
Roy <roy@gastom.com.tw>
2025-04-09 17:31:33 +0800

    增加vercel部署套件與設定

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
 import tailwindcss from '@tailwindcss/vite'
+import { vercelEdgeAdapter } from '@builder.io/qwik-city/adapters/vercel-edge/vite';
+
 
 type PkgDep = Record<string, string>;
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
@@ -22,7 +24,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), tailwindcss() ],
+    plugins: [vercelEdgeAdapter(), qwikCity(), qwikVite(), tsconfigPaths(), tailwindcss() ],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
